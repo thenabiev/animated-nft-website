0f21e8047726c0d7e878f78f582657cbc7267348
thenabiev <sahinnabee@gmail.com>
2023-03-02 11:31:31 +0400

    .

---
+++ app.js
@@ -0,0 +1,91 @@
+const hamburger=document.querySelector(".hamburger");
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    document.querySelector('.nav-ul').classList.toggle('active')
+    if(hamburger.classList.contains('active')){
+        document.body.style.position='fixed'
+    }else{
+        document.body.style.position='static'
+    }
+})
+
+
+
+document.querySelectorAll('.nav-a')
+.forEach(x=>x.addEventListener('click',()=>{
+    hamburger.classList.remove('active');
+    document.querySelector('.nav-ul').classList.remove('active')
+}))
+
+//SCROLL
+
+const root=document.documentElement;
+const marqueElementsDisplayed=getComputedStyle(root).getPropertyValue('--marque-elements-displayed')
+const marqueContent=document.querySelector('.marque-content')
+
+root.style.setProperty('--marque-elements', marqueContent.children.length);
+
+for(let i=0; i<marqueElementsDisplayed; i++){
+    marqueContent.appendChild(marqueContent.children[i].cloneNode(true))
+}
+
+const marqueAni=document.getElementById('marque-ani');
+const marqueFixed=document.getElementById('marque-fixed');
+
+function screenChange(e){
+    if(e.matches){
+        marqueAni.classList.add('hide');
+        marqueFixed.classList.remove('hide');
+    }else{
+        marqueFixed.classList.add('hide');
+        marqueAni.classList.remove('hide');
+    }
+}
+
+const mediaQuery=window.matchMedia('(min-width:800px)');
+
+mediaQuery.addListener(screenChange);
+
+screenChange(mediaQuery);
+
+// ANIMATIONS
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+const hiddenElements=document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el)=>{
+    observer.observe(el)
+})
+
+const hiddenBlur=document.querySelectorAll('.hiddenblr');
+
+hiddenBlur.forEach((el)=>{
+    observer.observe(el)
+})
+
+const slideRight=document.querySelectorAll('.slide-right');
+
+slideRight.forEach((el)=>{
+    observer.observe(el)
+})
+
+const slideLeft=document.querySelectorAll('.slide-left');
+
+slideLeft.forEach((el)=>{
+    observer.observe(el)
+})
+
+const enlarge=document.querySelectorAll('.enlarge');
+
+enlarge.forEach((el)=>{
+    observer.observe(el)
+})
